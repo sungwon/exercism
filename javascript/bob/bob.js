2dedb8af2928c750d5288fceb80ee9e7057faae9
sungwon <dokkaebi@gmail.com>
2014-10-12 14:39:28 -0400

    removed unused language directories from repository

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,14 +1,11 @@
 //
-// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
-// convenience to get you started writing code faster.
 //
 
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+	lackadaisacal_response = "whatever";
+	return lackadaisacal_response;
 };
 
 module.exports = Bob;
